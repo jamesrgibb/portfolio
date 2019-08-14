28c871ecba5851c7903f0e9e1a6ccc19bb4bb8d8
James Gibb <51032141+jamesrgibb@users.noreply.github.com>
2019-08-14 16:53:07 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function dropdown() {
-    var info = document.getElementById("golf-quiz");
-    if(info.style.display=== "none"){
-        info.style.display = "block";
-    } else {
-        info.style.display = "none";
-    }
-}
