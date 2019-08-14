1d84a9f6b564b6a98522cd43f47398b7d5cbb7db
James Gibb <51032141+jamesrgibb@users.noreply.github.com>
2019-08-14 16:51:10 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function dropdown() {
+    var info = document.getElementById("golf-quiz");
+    if(info.style.display=== "none"){
+        info.style.display = "block";
+    } else {
+        info.style.display = "none";
+    }
+}
